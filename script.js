182910497e22eefdd0b78c9655a845e3a1e6230b
henriB <henri.bretseau@gmail.com>
2020-11-07 13:03:51 +0100

    render bird

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const gravity = .5;
 const speed = 6.2;
 const size = [51,36];
 const jump = -11.5;
-const cTenth = (canvas.with /10);
+const cTenth = (canvas.width /10);
 
 let index = 0,
     bestScore = 0,
@@ -18,3 +18,12 @@ let index = 0,
     pipes =[],
     flight,
     flyHeight;
+
+    const render = () =>{
+        index++
+
+        ctx.drawImage(img,432, Math.floor((index %9)/3) *size[1], ...size, ((canvas.width /2) - size[0] /2), flyHeight, ...size);
+        flyHeight=(canvas.height /2) -(size[1]/2);
+        window.requestAnimationFrame(render);
+}
+img.onload = render;
