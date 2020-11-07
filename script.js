9b1bef3453f54309df814d326a8f860abad166b0
henriB <henri.bretseau@gmail.com>
2020-11-07 12:53:25 +0100

    General settings

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 const img = new Image()
 img.src = './media/flappy-bird-set.png';
+
+// GENERAL SETTINGS
+let gamePlaying = false;
+const gravity = .5;
+const speed = 6.2;
+const size = [51,36];
+const jump = -11.5;
+const cTenth = (canvas.with /10);
+
+let index = 0,
+    bestScore = 0,
+    currentScore = 0,
+    pipes =[],
+    flight,
+    flyHeight;
