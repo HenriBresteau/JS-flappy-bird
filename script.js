787da58f870cc78a6ac4de44d1f5ccabeeda548d
henriB <henri.bretseau@gmail.com>
2020-11-07 13:08:54 +0100

    render background

--- script.js
+++ script.js
@@ -22,6 +22,10 @@ let index = 0,
     const render = () =>{
         index++
 
+        // BACKGROUD
+        ctx.drawImage(img, 0,0, canvas.width, canvas.height, -(( index *(speed /2)) % canvas.width)+ canvas.width, 0 , canvas.width,canvas.height);
+        ctx.drawImage(img, 0,0, canvas.width, canvas.height, -(( index *(speed /2)) % canvas.width), 0 , canvas.width,canvas.height); 
+
         ctx.drawImage(img,432, Math.floor((index %9)/3) *size[1], ...size, ((canvas.width /2) - size[0] /2), flyHeight, ...size);
         flyHeight=(canvas.height /2) -(size[1]/2);
         window.requestAnimationFrame(render);
